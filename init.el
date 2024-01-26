0ddbf6a2f573ddec171f155fc570cf4e431d2c5b
Lu <gravitydiv10@protonmail.com>
2024-01-26 21:21:58 +0800

    old emacs need install modus theme from elpa

--- init.el
+++ init.el
@@ -8,6 +8,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f5661fd54b1e60a4ae373850447efc4158c23b1c7c9d65aa1295a606278da0f8" default))
  '(display-battery-mode t)
  '(display-time-mode t)
  '(inhibit-startup-screen t)
